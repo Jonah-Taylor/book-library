1e4d2706dfa55a6f3fcc33b2f09da6a69a8f1f36
Jonah Taylor <jt3438@drexel.edu>
2025-05-25 15:30:21 -0400

    Added better book functionality

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ let books = [];
 
 let bookDisplay = document.querySelector(".book-container");
 let searchFilter = document.querySelector(".search-bar");
+let addBook = document.querySelector(".add-book");
+let form = document.querySelector(".add-book-form");
 
-function Book(title, author, pages, read, imgPath = "Images/no-cover.jpg") {
+function Book(
+    title,
+    author,
+    pages,
+    readStatus,
+    imgPath = "Images/no-cover.jpg",
+) {
     if (!new.target) {
         throw Error("You must use 'new'!");
     }
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.readStatus = readStatus;
     this.imgPath = imgPath;
     this.info = function () {
-        let desc = title + " by " + author + ", " + pages + " pages, ";
-        if (read) {
-            desc += "was read";
-        } else {
-            desc += "not read yet";
-        }
+        let desc = "Written by " + author + " and has " + pages + " pages.";
         return desc;
     };
 }
@@ -27,10 +30,10 @@ function addBookToLibrary(
     name,
     author,
     pages,
-    read,
+    readStatus,
     imgPath = "Images/no-cover.jpg",
 ) {
-    books.push(new Book(name, author, pages, read, imgPath));
+    books.push(new Book(name, author, pages, readStatus, imgPath));
 }
 
 function updateBookList() {
@@ -60,6 +63,58 @@ function updateBookList() {
             title.innerText = book.title;
             bookDiv.appendChild(title);
 
+            // Add author & pages
+            const info = document.createElement("p");
+            info.classList.add("book-info");
+            info.innerText = book.info();
+            bookDiv.appendChild(info);
+
+            // Add status
+            const status = document.createElement("div");
+            status.classList.add("book-status");
+
+            // Add delete
+            const deleteButton = document.createElement("img");
+            deleteButton.src = "Images/close.svg";
+            deleteButton.classList.add("book-delete");
+            status.appendChild(deleteButton);
+            deleteButton.addEventListener("click", () => {
+                bookDiv.remove();
+                const index = books.indexOf(book);
+                if (index !== -1) books.splice(index, 1);
+            });
+
+            const statusLabel = document.createElement("label");
+            statusLabel.innerText = "Status";
+            statusLabel.htmlFor = "reading-status";
+            status.appendChild(statusLabel);
+
+            const statusInput = document.createElement("select");
+            statusInput.id = "reading-status";
+            statusInput.name = "status";
+
+            const wasRead = document.createElement("option");
+            wasRead.value = "yes";
+            wasRead.innerText = "Was Read";
+            statusInput.appendChild(wasRead);
+
+            const currentlyReading = document.createElement("option");
+            currentlyReading.value = "currently";
+            currentlyReading.innerText = "Currently Reading";
+            statusInput.appendChild(currentlyReading);
+
+            const notRead = document.createElement("option");
+            notRead.value = "no";
+            notRead.innerText = "Not Read";
+            statusInput.appendChild(notRead);
+
+            statusInput.addEventListener("change", (e) => {
+                book.readStatus = e.target.value;
+            });
+            statusInput.value = book.readStatus;
+
+            status.appendChild(statusInput);
+            bookDiv.appendChild(status);
             bookDisplay.appendChild(bookDiv);
         }
     }
@@ -70,7 +125,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "The Hobbit",
         "J.R.R. Tolkien",
         295,
-        false,
+        "yes",
         "Images/the-hobbit.jpg",
     );
 
@@ -78,20 +133,23 @@ document.addEventListener("DOMContentLoaded", () => {
         "Lord of the Flies",
         "William Golding",
         224,
-        false,
+        "no",
         "Images/lord-of-the-flies.jpg",
     );
 
-    addBookToLibrary("1984", "George Orwell", 328, false, "Images/1984.jpg");
+    addBookToLibrary("1984", "George Orwell", 328, "yes", "Images/1984.jpg");
     addBookToLibrary(
         "Frankenstein",
         "Mary Shelley",
         280,
-        true,
+        "currently",
         "Images/frankenstein.jpg",
     );
     updateBookList();
     searchFilter.addEventListener("keyup", (event) => {
         updateBookList();
+    });
+    addBook.addEventListener("click", (e) => {
+        form.showModal();
     });
 });
